809eddd66aa3515d3fab75b7e97b61bc47d8cfba
emiliano <emiliontn@hotmail.com>
2023-05-30 01:56:15 -0600

    actualizacion de login

--- src/components/Like.js
+++ src/components/Like.js
@@ -17,11 +17,86 @@ function saveLike(e, status) {
      e.preventDefault();
  }
 
+function saveEncanta(e, status) {
+
+
+  let data = {
+    id: 0,
+    status: status
+  };
+
+  console.log(JSON.stringify(data));
+
+  KafkaService.reaction("la-imagen-me-gusta");
+  e.preventDefault();
+}
+
+function savedivierte(e, status) {
+
+
+  let data = {
+    id: 0,
+    status: status
+  };
+
+  console.log(JSON.stringify(data));
+
+  KafkaService.reaction("que-gracioso");
+  e.preventDefault();
+}
+
+function savesorprendido(e, status) {
+
+
+  let data = {
+    id: 0,
+    status: status
+  };
+
+  console.log(JSON.stringify(data));
+
+  KafkaService.reaction("que-sorpresa");
+  e.preventDefault();
+}
+
+function savetriste(e, status) {
+
+
+  let data = {
+    id: 0,
+    status: status
+  };
+
+  console.log(JSON.stringify(data));
+
+  KafkaService.reaction("estoy-triste");
+  e.preventDefault();
+}
+
+function saveenojado(e, status) {
+
+
+  let data = {
+    id: 0,
+    status: status
+  };
+
+  console.log(JSON.stringify(data));
+
+  KafkaService.reaction("estoy-enfadado");
+  e.preventDefault();
+}
+
 
 function ReactionsComponent() {
   return (
     <div class="reactions">
-      <div class="reaction reaction-like"></div>
+      <div className="reaction reaction-like" onClick={(e) => {
+        e.preventDefault();
+        saveEncanta(e, 1);
+
+      }}>
+      </div>
 
       <div className="reaction reaction-love" onClick={(e) => {
         e.preventDefault();
@@ -31,10 +106,34 @@ function ReactionsComponent() {
       </div>
 
 
-      <div class="reaction reaction-haha"></div>
-      <div class="reaction reaction-wow"></div>
-      <div class="reaction reaction-sad"></div>
-      <div class="reaction reaction-angry"></div>
+      <div class="reaction reaction-haha"onClick={(e) => {
+        e.preventDefault();
+        savedivierte(e, 1);
+
+      }}>
+      </div>
+
+      <div class="reaction reaction-wow" onClick={(e) => {
+        e.preventDefault();
+        savesorprendido(e, 1);
+
+      }}>
+      </div>
+
+      <div class="reaction reaction-sad" onClick={(e) => {
+        e.preventDefault();
+        savetriste(e, 1);
+
+      }}>
+      </div>
+
+      <div class="reaction reaction-angry" onClick={(e) => {
+        e.preventDefault();
+        saveenojado(e, 1);
+
+      }}>
+      </div>
+
     </div>
   );
 }
